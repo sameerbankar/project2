a68bdc7e39cfe9b15eca57a289bc63a3c217dd2a
sameerb <bankar.samir@gmail.com>
2022-10-12 17:24:06 -0400

    added the live clock

--- welcome.py
+++ welcome.py
@@ -5,8 +5,14 @@ window = Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
 
-label = Label(window, text = "Welcome", font = ("Arial  ", 78, "bold"), bg= "steelblue", fg = 'white')
+label = Label(window, font = ("Arial  ", 78, "bold"), bg= "steelblue", fg = 'white')
 label.pack(pady =100)
 
+from datetime import datetime
+def clock():
+    time = datetime.now().strftime("%H:%M:%S")
+    label.configure(text=time)
+    label.after(500,clock)
 
+clock()
 window.mainloop()
